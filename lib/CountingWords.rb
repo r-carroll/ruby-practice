02ca8e02349aad01582a74d5a1ab8ea4ca1b3602
r-carroll <ryan8440@gmail.com>
2022-08-29 15:23:50 -0400

    adding word counting code:

--- lib/CountingWords.rb
+++ lib/CountingWords.rb
@@ -4,5 +4,20 @@ require_relative "CountingWords/version"
 
 module CountingWords
   class Error < StandardError; end
-  # Your code goes here...
+  # Reference and read the file
+  path = File.join(__dir__, 'resources/speech.txt');
+  file = File.open(path)
+  wordHash = {}
+  fileData = file.read
+  fileData = fileData.downcase
+  wordArray = fileData.split(' ')
+  wordArray.each { |word| 
+   if wordHash[word]
+    wordHash[word] = wordHash[word] + 1
+   elsif 
+    wordHash[word] = 1
+   end
+  }
+  file.close
+  puts wordHash
 end
